9e92edb786dc1d5160af6d07975a7c001bef9e0d
Zsolt Balvanyos <zsolt.balvanyos@gmail.com>
2020-11-16 14:26:58 +0000

    Fix formatting

--- upcloud/interface/host.go
+++ upcloud/interface/host.go
@@ -1,12 +1,12 @@
 package interfaces
 
 import (
-  "github.com/UpCloudLtd/upcloud-go-api/upcloud"
-  "github.com/UpCloudLtd/upcloud-go-api/upcloud/request"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud/request"
 )
 
 type Host interface {
-  GetHosts() (*upcloud.Hosts, error)
-  GetHostDetails(r *request.GetHostDetailsRequest) (*upcloud.Host, error)
-  ModifyHost(r *request.ModifyHostRequest) (*upcloud.Host, error)
+	GetHosts() (*upcloud.Hosts, error)
+	GetHostDetails(r *request.GetHostDetailsRequest) (*upcloud.Host, error)
+	ModifyHost(r *request.ModifyHostRequest) (*upcloud.Host, error)
 }
