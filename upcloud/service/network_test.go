d59a1532707b4ed8fdd6fb9216af2c4316c3b399
Matias Lahti <matias.lahti@upcloud.com>
2021-05-21 14:41:44 +0300

    test(network): improve and fix TestCreateTwoNetworksTwoServersAndARouter and update fixture

--- upcloud/service/network_test.go
+++ upcloud/service/network_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"testing"
+	"time"
 
 	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
 	"github.com/UpCloudLtd/upcloud-go-api/upcloud/request"
@@ -286,6 +287,7 @@ func TestCreateModifyDeleteRouter(t *testing.T) {
 //     - adds network interfaces in each one server in each network
 //     - verifies the network details in the interfaces is correct
 //     - verifies the servers can be found in the network details
+//     - detaches one of the routers and verifies it was detached
 //     - deletes the servers, the routers and the networks
 func TestCreateTwoNetworksTwoServersAndARouter(t *testing.T) {
 	record(t, "createtwonetworkstwoserversandarouter", func(t *testing.T, svc *Service) {
@@ -425,6 +427,7 @@ func TestCreateTwoNetworksTwoServersAndARouter(t *testing.T) {
 				found = true
 			}
 		}
+		assert.True(t, found)
 
 		network2Details, err := svc.GetNetworkDetails(&request.GetNetworkDetailsRequest{
 			UUID: network2.UUID,
@@ -437,6 +440,18 @@ func TestCreateTwoNetworksTwoServersAndARouter(t *testing.T) {
 				found = true
 			}
 		}
+		assert.True(t, found)
+
+		// try detaching a router
+		err = svc.DetachNetworkRouter(&request.DetachNetworkRouterRequest{NetworkUUID: network1.UUID})
+		require.NoError(t, err)
+		assert.Eventually(t, func() bool {
+			details, err := svc.GetNetworkDetails(&request.GetNetworkDetailsRequest{
+				UUID: network1.UUID,
+			})
+			require.NoError(t, err)
+			return err == nil && details.Router == ""
+		}, 15*time.Second, time.Second)
 
 		err = deleteServer(svc, serverDetails1.UUID)
 		require.NoError(t, err)
