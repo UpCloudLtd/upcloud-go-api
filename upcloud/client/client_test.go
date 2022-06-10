684a519aaf0679d104e286b0eb094a99d003498c
Pekka Nurmi <pekka.nurmi@upcloud.com>
2022-06-10 13:59:28 +0300

    feat(client-context): disable default timeout
    Disable default timeout when using context aware client
    as it might result in unexpected behaviour when using context cancellation.

--- upcloud/client/client_test.go
+++ upcloud/client/client_test.go
@@ -1,11 +1,13 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"testing"
 	"time"
 
+	globals "github.com/UpCloudLtd/upcloud-go-api/v4/internal"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,6 +17,20 @@ func TestClientBaseURL(t *testing.T) {
 	assert.Equal(t, DefaultAPIBaseURL, clientBaseURL("http://"))
 	assert.Equal(t, "http://127.0.0.1", clientBaseURL("http://127.0.0.1"))
 	assert.Equal(t, "https://127.0.0.1", clientBaseURL("https://127.0.0.1"))
+}
+
+func TestClientTimeout(t *testing.T) {
+	var u, p string
+	c1 := New(u, p)
+	assert.Equal(t, time.Second*DefaultTimeout, c1.GetTimeout())
+	c2 := NewWithHTTPClient(u, p, http.DefaultClient)
+	assert.Equal(t, time.Second*DefaultTimeout, c2.GetTimeout())
+}
+
+func TestClientUserAgent(t *testing.T) {
+	var u, p string
+	c1 := New(u, p)
+	assert.Equal(t, fmt.Sprintf("upcloud-go-api/%s", globals.Version), c1.UserAgent)
 }
 
 func ExampleNew() {
