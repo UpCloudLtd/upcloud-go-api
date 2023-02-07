b2bc7ba477aeec37d2bef2c1925c30a37575b95e
kamil cybulski <kamillcybulski+2@gmail.com>
2023-02-07 12:09:16 +0200

    feat(errors): add error codes list

--- upcloud/problem.go
+++ upcloud/problem.go
@@ -14,10 +14,17 @@ type Problem struct {
 	// InvalidParams if set, is a list of ProblemInvalidParam describing a specific part(s) of the request
 	// that caused the problem
 	InvalidParams []ProblemInvalidParam `json:"invalid_params,omitempty"`
-	// CorrelationID is an unique string that identifies the request that caused the problem
+	// CorrelationID is a unique string that identifies the request that caused the problem
+	// Please note that it is not always available
 	CorrelationID string `json:"correlation_id,omitempty"`
 	// HTTP Status code
 	Status int `json:"status"`
+}
+
+// ProblemInvalidParam is a type describing extra information in the Problem type's InvalidParams field.
+type ProblemInvalidParam struct {
+	Name   string `json:"name"`
+	Reason string `json:"reason"`
 }
 
 func (p *Problem) Error() string {
@@ -34,8 +41,37 @@ func (p *Problem) Error() string {
 	return sb.String()
 }
 
-// ProblemInvalidParam is a type describing extra information in the Problem type's InvalidParams field.
-type ProblemInvalidParam struct {
-	Name   string `json:"name"`
-	Reason string `json:"reason"`
+// MatchesProblemType is a helper method for comparing a `Problem.Type` field against a problem type constants defined in this package.
+// Please note that while you can compare the `Type` field directly to those constants, it is not recommended and might not always work reliably.
+// Examples:
+//
+//	res, err := svc.GetServerDetails(ctx, &req)
+//
+//	var problem *upcloud.Problem
+//	if errors.As(err, &problem) {
+//		// This is the recommended way
+//		if problem.MatchesProblemType(upcloud.ProblemTypeResourceAlreadyExists) {
+//			handleStuff(problem)
+//		}
+//
+//		// This might work, but is not guaranteed and not recommended
+//		if problem.Type == upcloud.ProblemTypeResourceAlreadyExists {
+//			handleStuff(problem)
+//		}
+//	}
+func (p *Problem) MatchesProblemType(problemType string) bool {
+	// Type is a URL, we need to extract meaningful fragment from it for comparison purposes
+	if strings.Contains(p.Type, "https://") {
+		parts := strings.SplitN(p.Type, "#", 2)
+
+		if len(parts) < 2 {
+			return false
+		}
+
+		return strings.Replace(parts[1], "ERROR_", "", 1) == problemType
+	}
+
+	// Type is just bare problem type string (this happens when error returned from the API is not in the json+problem format)
+	// We can just compare
+	return p.Type == problemType
 }
--- upcloud/problem_test.go
+++ upcloud/problem_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestProblem(t *testing.T) {
+func TestProblemUnmarshal(t *testing.T) {
 	p := Problem{}
 	err := json.Unmarshal([]byte(`
 	{
@@ -30,4 +30,24 @@ func TestProblem(t *testing.T) {
 	assert.Equal(t, 400, p.Status)
 	assert.Equal(t, "Backend doesn't exist.", p.InvalidParams[0].Reason)
 	assert.Equal(t, "default_backend", p.InvalidParams[0].Name)
+}
+
+func TestProblemTypeMatching(t *testing.T) {
+	p := Problem{
+		Type: "https://api.upcloud.com/1.3/errors#ERROR_RESOURCE_ALREADY_EXISTS",
+	}
+	assert.True(t, p.MatchesProblemType(ProblemTypeResourceAlreadyExists))
+	assert.True(t, p.MatchesProblemType("RESOURCE_ALREADY_EXISTS"))
+	assert.False(t, p.MatchesProblemType(ProblemTypeAuthenticationFailed))
+
+	p.Type = "https://api.upcloud.com/1.3/errors#ERROR_AUTHENTICATION_FAILED"
+	assert.True(t, p.MatchesProblemType(ProblemTypeAuthenticationFailed))
+
+	p.Type = "GROUP_NOT_FOUND"
+	assert.True(t, p.MatchesProblemType(ProblemTypeGroupNotFound))
+
+	p.Type = "SERVER_NOT_FOUND"
+	assert.True(t, p.MatchesProblemType(ProblemTypeServerNotFound))
+	assert.True(t, p.MatchesProblemType("SERVER_NOT_FOUND"))
+	assert.False(t, p.MatchesProblemType("SOME_UNKNOWN_ERROR_SO_THIS_SHOULD_FAIL"))
 }
