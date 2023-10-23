0389a7307997c542adf36f74da92e513f2615c8a
Toni Kangas <toni.kangas@upcloud.com>
2023-10-23 15:07:37 +0300

    chore: add script to automatically update version in user-agent

---
+++ scripts/update-user-agent-version.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+version_re="[0-9]+\.[0-9]+\.[0-9]+"
+
+changelog=$(grep -E -m 1 "##.*$version_re.*" CHANGELOG.md | grep -Eo "$version_re");
+user_agent=$(grep -E -m 1 "Version.*$version_re.*" upcloud/client/client.go | grep -Eo "$version_re")
+
+if [ "$changelog" = "$user_agent" ]; then
+    exit 0;
+fi;
+
+latest=$(echo -e "$changelog\n$user_agent" | sort -V | tail -n1)
+
+sed -Ei "s/(##.*)$changelog(.*)/\1$latest\2/" CHANGELOG.md;
+sed -Ei "s/(.*Version.*\")$user_agent(\".*)/\1$latest\2/" upcloud/client/client.go;
